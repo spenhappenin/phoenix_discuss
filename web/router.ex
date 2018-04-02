a6db22ebcd834ff099d5cb0d1e0eadb472292b9f
Spencer Richards <spencer.richards7@gmail.com>
2018-04-02 16:41:38 -0600

    complete crud actions for topics

--- web/router.ex
+++ web/router.ex
@@ -19,6 +19,12 @@ defmodule Discuss.Router do
     get "/", TopicController, :index
     get "/topics/new", TopicController, :new
     post "/topics", TopicController, :create
+    get "/topics/:id/edit", TopicController, :edit
+    put "/topics/:id", TopicController, :update
+    delete "/topics/:id", TopicController, :delete
+    # OR
+    # resources "/", TopicController
+
   end
 
   # Other scopes may use custom stacks.
